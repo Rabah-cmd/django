4b4bdc0593c6c10849e23e71dd9d491e6b9854fa
Rabah-cmd <rab6@hotmail.co.uk>
2025-07-01 15:24:18 +0100

    Add Procfile to configure Gunicorn for the Django application

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6opa0*8=e_0^t1bs2a5sy9%!&naha_6mx5w)z&s)_b7&_9o#9$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-6opa0*8=e_0^t1bs2a5sy9%!&naha_6mx5w)z&s)_b7&_9o#9$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
